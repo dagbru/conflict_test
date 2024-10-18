6b6141f2e1a10398ada165a82587f45a091f4621
dagbru <dag.sorensen@gmail.com>
2024-10-18 11:03:40 +0200

    Update deleteController.js

--- js/deleteController.js
+++ js/deleteController.js
@@ -1,7 +1,12 @@
 function deletePerson() {
     if (!model.inputs.delete.areYouSure) return;
     const index = findPersonIndexById(model.inputs.delete.personId);
+    // This is nice
     model.people.splice(index, 1);
-    model.app.page = 'search';
+    model.app.page = 's';
+
+    if(model.app.page == 's') {
+        console.log('yass');
+    }
     updateView();
 }
