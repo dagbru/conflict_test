fc984126ebd3830d1473b902aa597c918ac77995
Dag Sorensen <dag.sorensen@gmail.com>
2024-10-18 11:04:28 +0200

    test

--- js/deleteController.js
+++ js/deleteController.js
@@ -1,7 +1,10 @@
 function deletePerson() {
-    if (!model.inputs.delete.areYouSure) return;
+    if (!model.inputs.delete.areYouSure) {
+        return;
+    } 
     const index = findPersonIndexById(model.inputs.delete.personId);
     model.people.splice(index, 1);
+    // Maybe we should rethink this?
     model.app.page = 'search';
     updateView();
 }
